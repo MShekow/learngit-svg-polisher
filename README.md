14e8b53a4439c5244ffd24556342657d777c0a75
MShekow <s2mashek@uni-bonn.de>
2022-12-30 12:37:17 +0100

    Update code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# learngit-svg-polisher
-Python script that fixes some issues of SVGs exported by the Firefox add-on "svg-screenshots" for Git trees generated on learngitbranching.js.org
+# LearnGit SVG exporter
+
+This simple Python script fixes some issues of SVGs exported by the Firefox
+add-on [svg-screenshots](https://github.com/felixfbecker/svg-screenshots) that you make on Git trees generated
+on https://learngitbranching.js.org/
+
+The problem is that the SVG produced by the add-on has some errors.
+
+The concrete fixes are:
+
+- Remove surrounding clutter (including the blue background) -> the final SVG has a transparent background
+- Change color from white to black for the edge-borders and node-text-labels
+
+## Examples
+
+| Input   |      Cleaned output      |
+|----------|:-------------:|
+|![](bad_input.svg) |  ![](processed_output.svg) |
+
+## Usage
+
+Run `pip3 install beautifulsoup4 lxml` (preferrably in a virtual-env!) before running the `main.py` script.
---
+++ main.py
@@ -0,0 +1,64 @@
+"""
+Fixes SVGs exported by the add-on https://github.com/felixfbecker/svg-screenshots
+Specifically:
+- Remove surrounding clutter (including the blue background) -> the final SVG has a transparent background
+- Change color from white to black for the edge-borders and node-text-labels
+
+Run "pip3 install beautifulsoup4 lxml" before running this script.
+"""
+
+from pathlib import Path
+
+from bs4 import BeautifulSoup, Tag
+
+
+def extract_real_graph(svg_root: Tag, actual_graph: Tag):
+    svg_root.insert(0, actual_graph)
+    for child in svg_root.children:
+        keep = False
+        try:
+            if child.name == "g" and child.attrs["class"][0] == "svg-content":
+                keep = True
+        except:
+            pass
+
+        if not keep:
+            child.extract()
+
+
+def fix_colors(svg_root: Tag):
+    for tag_name in ["circle", "path", "rect"]:
+        for node in svg_root.find_all(tag_name):
+            node.attrs["stroke"] = "rgb(0, 0, 0)"
+
+    for text in svg_root.find_all("tspan"):
+        text.attrs["fill"] = "rgb(0, 0, 0)"
+
+    # Fix arrow-tips
+    for arrow in svg_root.find_all("path"):
+        if arrow.attrs["fill"] == "rgb(51, 51, 51)":
+            arrow.attrs["fill"] = "rgb(0, 0, 0)"
+
+
+def save_to_file(svg_root: Tag, file_name: str) -> None:
+    f = Path(".") / file_name
+    f.write_text(str(svg_root).replace("viewbox", "viewBox"))
+
+
+if __name__ == '__main__':
+    # tree = ET.parse('index.svg')
+    # root = tree.getroot()
+    f = Path(".") / "index.svg"
+    with f.open() as fp:
+        b = BeautifulSoup(fp, "lxml")
+
+    actual_graph = b.find("g", {"class": "svg-content"})
+
+    svg_root = b.find("svg")
+
+    while len(svg_root.contents) > 1:
+        extract_real_graph(svg_root, actual_graph)
+        
+    fix_colors(svg_root)
+
+    save_to_file(svg_root, "out.svg")
